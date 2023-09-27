2f5bce5aab31db2cda3eb4a49f4b7dcf444d9ed9
benjaAguilar <benjaaguilar02@gmail.com>
2023-09-27 16:23:28 -0300

    solved 04_removeFromArray
    used for() and for of loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(numArray, ...restElements){
+
+    for(let i = 0; i <= numArray.length - 1; i++){
+
+        for(let element of restElements){
+
+            if(numArray[i] === element){
+                numArray.splice(i, 1);
+                i = 0;
+                
+            }
+        
+        }
+    }
+
+    return numArray
 
 };
 
