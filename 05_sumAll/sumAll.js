7955be0240580f64dc1716fd6720badaef910fae
benjaAguilar <benjaaguilar02@gmail.com>
2023-09-27 17:28:52 -0300

    solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let sum = 0;
+
+    if(firstNum > lastNum){
+        let saveLastNum = lastNum;
+        lastNum = firstNum;
+        firstNum = saveLastNum;
+
+    } else if(firstNum < 0 || lastNum < 0){
+        return "ERROR"
+
+    } else if(typeof firstNum != "number" || typeof lastNum != "number"){
+        return "ERROR"
+
+    }
+
+    for(let i = firstNum; i <= lastNum; i++){
+        sum += i; 
+    }
+
+    return sum;
 
 };
 
