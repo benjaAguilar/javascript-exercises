c1bc4a370e6274e4f4f94d8255f84c4c5bcc29ce
benjaAguilar <benjaaguilar02@gmail.com>
2023-10-07 17:10:47 -0300

    solved 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let str = string.toLowerCase();
+
+    if(str.includes(", ")){
+        str = str.split(',').join('');
+        str = str.split(' ').join('');
+
+    } else{
+        str = str.split(' ').join('');
+
+    }
+    
+    let arr = str.split('');
+
+    if(str.includes("!") || str.includes(".")){
+        arr.pop();
+    }
+    
+    let reversed = arr.slice().reverse().join('');
+    console.log(reversed);
+    console.log(arr.join(''));
+
+    return (reversed === arr.join('')) ? true: false;
 
 };
 
