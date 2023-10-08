8384de41561e19cb7733e1fa9faca61724821511
benjaAguilar <benjaaguilar02@gmail.com>
2023-10-08 09:45:25 -0300

    solved execise 10_fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(fn) {
+    let fibonacciNumbers = [0, 1, 1, 2];
 
+    for(let i = 0; i <= fn; i++){
+        if(i <= 3){
+            continue;
+
+        }
+        let index1 = i - 1;
+        let index2 = i - 2;
+
+        let number = fibonacciNumbers[index1] + fibonacciNumbers[index2];
+
+        fibonacciNumbers.push(number);
+    }
+    return (fibonacciNumbers[fn] === undefined) ? "OOPS": fibonacciNumbers[fn];
 };
 
 // Do not edit below this line
